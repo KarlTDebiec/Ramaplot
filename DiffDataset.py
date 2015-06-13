Fixed masking of unfavorable regions; now looks much better

DiffDataset.py
@@ -18,8 +18,7 @@ class DiffDataset(object):
 
     """
 
-    def __init__(self, dataset_1, dataset_2, max_fe=5, verbose=1,
-        debug=1, **kwargs):
+    def __init__(self, dataset_1, dataset_2, verbose=1, debug=1, **kwargs):
         """
         Initializes dataset.
 
@@ -53,32 +52,21 @@ class DiffDataset(object):
         self.x_bins = dataset_1.x_bins
         self.y_bins = dataset_1.y_bins
         self.free_energy = dataset_1.free_energy - dataset_2.free_energy
-        if max_fe is not None:
-            self.free_energy[dataset_1.free_energy >= max_fe] = np.nan
         self.probability = dataset_1.probability - dataset_2.probability
 
-        # Format contour settings
-        if hasattr(dataset_1, "contour_x_centers"):
-            self.contour_x_centers = dataset_1.contour_x_centers
-        if hasattr(dataset_1, "contour_y_centers"):
-            self.contour_y_centers = dataset_1.contour_y_centers
-        if (hasattr(dataset_1, "contour_free_energy")
-        and hasattr(dataset_2, "contour_free_energy")):
-            self.contour_free_energy = (dataset_1.contour_free_energy
-                                     -  dataset_2.contour_free_energy)
-            if max_fe is not None:
-                self.contour_free_energy[np.logical_and(
-                  dataset_1.contour_free_energy >= max_fe,
-                  dataset_2.contour_free_energy >= max_fe)] = np.nan
-
-        # Format heatmap settings
-        if (hasattr(dataset_1, "imshow_free_energy")
-        and hasattr(dataset_2, "imshow_free_energy")):
-            self.imshow_free_energy = (dataset_1.imshow_free_energy
-                                    -  dataset_2.imshow_free_energy)
-            if max_fe is not None:
-                self.imshow_free_energy[np.logical_and(
-                  dataset_1.imshow_free_energy >= max_fe,
-                  dataset_2.imshow_free_energy >= max_fe)] = np.nan
-        if hasattr(dataset_1, "imshow_extent"):
-            self.imshow_extent = dataset_1.imshow_extent
+        # Prepare mask
+        #   Values that are NaN are all masked, as are values that are
+        #   masked in both source datasets; two boolean arrays are
+        #   prepared manually; np.logical_and() and np.logical_or()
+        #   yield the same result when passed masked arrays, and are
+        #   either broken or not intended to be used for this purpose,
+        #   and np.ma.mask_or() does not appear to be usable either.
+        temp1 = np.zeros_like(dataset_1.free_energy)
+        temp2 = np.zeros_like(dataset_2.free_energy)
+        temp1[dataset_1.mask != 1] = 1
+        temp2[dataset_2.mask != 1] = 1
+        self.mask = np.ma.masked_where(
+          np.logical_and(
+            np.logical_not(np.isnan(self.free_energy)),
+            np.logical_or(temp1, temp2)),
+          np.ones_like(self.free_energy))

NDRDDataset.py
@@ -27,7 +27,8 @@ class NDRDDataset(object):
     <http://dunbrack.fccc.edu/ndrd>_
     """
 
-    def __init__(self, infile, selection="ALA", **kwargs):
+    def __init__(self, infile, selection="ALA", loop_edges=True, max_fe=None,
+        **kwargs):
         """
         Initializes dataset.
 
@@ -218,45 +219,56 @@ class NDRDDataset(object):
         self.data = dist
         self.x_centers = np.unique(self.data["x"])
         self.y_centers = np.unique(self.data["y"])
+        self.free_energy = np.zeros((self.x_centers.size, self.y_centers.size),
+                                    np.float) * np.nan
+        self.probability = np.zeros((self.x_centers.size, self.y_centers.size),
+                                    np.float) * np.nan
+        for index, x, y, probability, free_energy in self.data.itertuples():
+            x_index = np.where(self.x_centers == x)[0][0]
+            y_index = np.where(self.y_centers == y)[0][0]
+            self.free_energy[x_index, y_index] = free_energy
+            self.probability[x_index, y_index] = probability
+        self.free_energy -= np.nanmin(self.free_energy)
+
         self.x_width = np.mean(self.x_centers[1:] - self.x_centers[:-1])
         self.y_width = np.mean(self.y_centers[1:] - self.y_centers[:-1])
+
+        # Loop data to allow contour lines to be drawn to edges
+        if loop_edges:
+            self.x_centers = np.concatenate(
+                               ([self.x_centers[0]-self.x_width],
+                                 self.x_centers,
+                                [self.x_centers[-1]+self.x_width]))
+            self.y_centers = np.concatenate(
+                               ([self.y_centers[0]-self.y_width],
+                                 self.y_centers,
+                                [self.y_centers[-1]+self.y_width]))
+            temp = np.zeros((self.x_centers.size, self.y_centers.size))*np.nan
+            temp[1:-1,1:-1]  = self.free_energy
+            temp[1:-1,-1]    = self.free_energy[:,0]
+            temp[-1,1:-1]    = self.free_energy[0,:]
+            temp[1:-1,0]     = self.free_energy[:,-1]
+            temp[0,1:-1]     = self.free_energy[-1,:]
+            temp[0,0]        = self.free_energy[-1,-1]
+            temp[-1,-1]      = self.free_energy[0,0]
+            temp[0,-1]       = self.free_energy[-1,0]
+            temp[-1,0]       = self.free_energy[0,-1]
+            self.free_energy = temp
+
         self.x_bins  = np.linspace(self.x_centers[0]  - self.x_width/2,
                                    self.x_centers[-1] + self.x_width/2,
                                    self.x_centers.size + 1)
         self.y_bins  = np.linspace(self.y_centers[0]  - self.y_width/2,
                                    self.y_centers[-1] + self.y_width/2,
                                    self.y_centers.size + 1)
-        self.free_energy = np.zeros((self.x_centers.size, self.y_centers.size),
-                                    np.float)
-        self.probability = np.zeros((self.x_centers.size, self.y_centers.size),
-                                    np.float)
-        self.free_energy[:] = np.nan
-        self.probability[:] = np.nan
-        for index, x, y, probability, free_energy in self.data.itertuples():
-            y_index = np.where(self.x_centers == x)[0][0]
-            x_index = np.where(self.y_centers == y)[0][0]
-            self.free_energy[x_index, y_index] = free_energy
-            self.probability[x_index, y_index] = probability
-        self.free_energy -= np.nanmin(self.free_energy)
 
-        # Format contour settings
-        self.contour_x_centers = np.zeros(self.x_centers.size + 1, np.float)
-        self.contour_x_centers[0:-1] = self.x_centers
-        self.contour_x_centers[-1] = self.contour_x_centers[-2] + self.x_width
-        self.contour_y_centers = np.zeros(self.y_centers.size + 1, np.float)
-        self.contour_y_centers[0:-1] = self.y_centers
-        self.contour_y_centers[-1] = self.contour_y_centers[-2] + self.y_width
-        self.contour_free_energy = np.zeros((self.x_centers.size + 1,
-                                             self.y_centers.size + 1),
-                                             np.float)
-        self.contour_free_energy[:] = np.nan
-        self.contour_free_energy[0:-1,0:-1] = self.free_energy
-        self.contour_free_energy[0:-1,-1]   = self.free_energy[:,0]
-        self.contour_free_energy[-1,0:-1]   = self.free_energy[0,:]
-        self.contour_free_energy[-1,-1]     = self.free_energy[0,0]
-        self.x_bins = np.append(self.x_bins, self.x_bins[-1] + self.x_width)
-        self.y_bins = np.append(self.y_bins, self.y_bins[-1] + self.y_width)
-
-        # Format heatmap settings
-        self.imshow_free_energy = self.contour_free_energy
-        self.imshow_extent = [-182.5, 182.5, 182.5, -182.5]
+        # Prepare mask
+        if max_fe is not None:
+            self.mask = np.ma.masked_where(np.logical_and(
+              self.free_energy <= max_fe,
+              np.logical_not(np.isnan(self.free_energy))),
+              np.ones_like(self.free_energy))
+        else:
+            self.mask = np.ma.masked_where(
+              np.logical_not(np.isnan(self.free_energy)),
+              np.ones_like(self.free_energy))

RamachandranFigureManager.py
@@ -39,29 +39,48 @@ class RamachandranFigureManager(FigureManager):
           ylabel_kw:
             va: center
         draw_dataset:
+          heatmap_kw:
+            cmap: hot
+            edgecolors: none
+            rasterized: True
+            vmin: 0
+            vmax: 5
           contour_kw:
             colors: '0.25'
             levels: [0, 1, 2, 3, 4, 5]
             linestyles: solid
-          imshow_kw:
-            cmap: 'hot'
-            interpolation: 'none'
+          mask_kw:
+            cmap: Greys_r
+            edgecolors: none
+            rasterized: True
             vmin: 0
-            vmax: 5
+            vmax: 1
+          outline_kw:
+            color: black
     """
 
     presets = """
       diff:
         help: Difference between two datasets
         draw_dataset:
-          contour_kw:
-            colors: '0.25'
-            levels: [-5,-4,-3,-2,-1,0,1,2,3,4,5]
-          imshow_kw:
+          heatmap_kw:
             cmap: 'seismic'
             vmin: -5
             vmax:  5
-          nan_outline: True
+          contour_kw:
+            levels: [-5,-4,-3,-2,-1,0,1,2,3,4,5]
+          load_kw:
+            max_fe: 5
+          mask: True
+          outline: True
+      right_title:
+        help: Additional subplot title on right side
+        draw_subplot:
+          y2ticks: [-180,-90,0,90,180]
+          y2ticklabels: []
+          y2label_kw:
+            labelpad: 20
+            rotation: 270
       poster:
         help: Single plot for poster (width = 4.6", height = 4.3")
         inherits: poster
@@ -206,8 +225,8 @@ class RamachandranFigureManager(FigureManager):
 
     @manage_defaults_presets()
     @manage_kwargs()
-    def draw_dataset(self, subplot, infile=None, kind="WHAM",
-        nan_to_max=False, nan_outline=False,**kwargs):
+    def draw_dataset(self, subplot, infile=None, kind="WHAM", heatmap=True,
+        nan_to_max=True, contour=True, mask=False, outline=False,**kwargs):
         import numpy as np
         import six
         from .myplotspec import get_color
@@ -241,83 +260,67 @@ class RamachandranFigureManager(FigureManager):
                 dataset_2 = parsers[kind[1]](infile=infile[1], **load_kw_2)
             dataset = DiffDataset(dataset_1, dataset_2)
 
-#        if nan_to_max:
-#            dataset.free_energy[np.isnan(dataset.free_energy)] = np.nanmax(
-#              dataset.free_energy)
+        # Draw heatmap
+        if heatmap:
+            heatmap_kw = kwargs.get("heatmap_kw", {}).copy()
+            heatmap_free_energy = dataset.free_energy.copy()
+            if nan_to_max and hasattr(dataset, "mask"):
+                heatmap_free_energy[np.isnan(heatmap_free_energy)] = np.nanmax(
+                  heatmap_free_energy)
+            subplot.pcolormesh(dataset.x_bins, dataset.y_bins,
+              heatmap_free_energy.T, zorder=0.1, **heatmap_kw)
 
-        # Configure contour settings
-        contour_kw = kwargs.get("contour_kw", {})
-        if hasattr(dataset, "contour_x_centers"):
-            contour_x_centers = dataset.contour_x_centers
-        else:
-            contour_x_centers = dataset.x_centers
-        if hasattr(dataset, "contour_y_centers"):
-            contour_y_centers = dataset.contour_y_centers
-        else:
-            contour_y_centers = dataset.y_centers
-        if hasattr(dataset, "contour_free_energy"):
-            contour_free_energy = dataset.contour_free_energy
-        else:
-            contour_free_energy = dataset.free_energy
-        if "levels" in kwargs:
-            contour_kw["levels"] = kwargs.pop("color")
-        elif "levels" not in contour_kw:
-            contour_kw["levels"] = range(0,
-              int(np.ceil(np.nanmax(dataset.free_energy))))
+        # Draw contour
+        if contour:
+            contour_kw = kwargs.get("contour_kw", {})
+            if "levels" in kwargs:
+                contour_kw["levels"] = kwargs.pop("color")
+            elif "levels" not in contour_kw:
+                contour_kw["levels"] = range(0,
+                  int(np.ceil(np.nanmax(dataset.free_energy))))
+            subplot.contour(dataset.x_centers, dataset.y_centers,
+              dataset.free_energy.T, zorder=0.2, **contour_kw)
 
-        # Configure heatmap settings
-        imshow_kw = kwargs.get("imshow_kw", {})
-        if "extent" in kwargs:
-            imshow_kw["extent"] = kwargs.pop("extent")
-        elif "extent" not in imshow_kw:
-            if hasattr(dataset, "imshow_extent"):
-                imshow_kw["extent"] = dataset.imshow_extent
-            else:
-                imshow_kw["extent"] = kwargs.get("extent",
-                  [np.min(dataset.x_bins), np.max(dataset.x_bins),
-                   np.max(dataset.y_bins), np.min(dataset.y_bins)])
-        if hasattr(dataset, "imshow_free_energy"):
-            imshow_free_energy = dataset.imshow_free_energy
-        else:
-            imshow_free_energy = dataset.free_energy
+        # Draw mask
+        if mask and hasattr(dataset, "mask"):
+            mask_kw = kwargs.get("mask_kw", {}).copy()
+            subplot.pcolormesh(dataset.x_bins, dataset.y_bins,
+              dataset.mask.T, zorder=0.3, **mask_kw)
 
-        # Plot
-        subplot.contour(contour_x_centers, contour_y_centers,
-          contour_free_energy, **contour_kw)
-        subplot.imshow(imshow_free_energy, **imshow_kw)
-
-        if nan_outline:
-            for x in range(imshow_free_energy.shape[0]):
-                for y in range(imshow_free_energy.shape[1]):
-                    if np.isnan(imshow_free_energy[x,y]):
-                        if     (x != 0
-                        and     y != imshow_free_energy.shape[1] - 1
-                        and not np.isnan(imshow_free_energy[x-1,y])):
+        # Draw outline
+        if outline and hasattr(dataset, "mask"):
+            outline_kw = kwargs.get("outline_kw", {}).copy()
+            for x in range(dataset.free_energy.shape[0]):
+                for y in range(dataset.free_energy.shape[1]):
+                    if not dataset.mask[x,y]:
+                        if (x != 0
+                        and y != dataset.mask.shape[1] - 1
+                        and dataset.mask[x-1,y]):
                             subplot.plot(
-                              [dataset.y_bins[y], dataset.y_bins[y+1]],
                               [dataset.x_bins[x], dataset.x_bins[x]],
-                              color="black")
-                        if     (x != imshow_free_energy.shape[0] - 1
-                        and     y != imshow_free_energy.shape[1] - 1
-                        and not np.isnan(imshow_free_energy[x+1,y])):
-                            subplot.plot(
                               [dataset.y_bins[y], dataset.y_bins[y+1]],
+                              zorder=0.4, **outline_kw)
+                        if (x != dataset.mask.shape[0] - 1
+                        and y != dataset.mask.shape[1] - 1
+                        and dataset.mask[x+1,y]):
+                            subplot.plot(
                               [dataset.x_bins[x+1], dataset.x_bins[x+1]],
-                              color="black")
-                        if     (x != imshow_free_energy.shape[0] - 1
-                        and     y != 0
-                        and not np.isnan(imshow_free_energy[x,y-1])):
+                              [dataset.y_bins[y],   dataset.y_bins[y+1]],
+                              zorder=0.4, **outline_kw)
+                        if (x != dataset.mask.shape[0] - 1
+                        and y != 0
+                        and dataset.mask[x,y-1]):
                             subplot.plot(
+                              [dataset.x_bins[x], dataset.x_bins[x+1]],
                               [dataset.y_bins[y], dataset.y_bins[y]],
-                              [dataset.x_bins[x], dataset.x_bins[x+1]],
-                              color="black")
-                        if     (x != imshow_free_energy.shape[0] - 1
-                        and     y != imshow_free_energy.shape[1] - 1
-                        and not np.isnan(imshow_free_energy[x,y+1])):
+                              zorder=0.4, **outline_kw)
+                        if (x != dataset.mask.shape[0] - 1
+                        and y != dataset.mask.shape[1] - 1
+                        and dataset.mask[x,y+1]):
                             subplot.plot(
-                              [dataset.y_bins[y+1], dataset.y_bins[y+1]],
                               [dataset.x_bins[x], dataset.x_bins[x+1]],
-                              color="black")
+                              [dataset.y_bins[y+1], dataset.y_bins[y+1]],
+                              zorder=0.4, **outline_kw)
 
 #################################### MAIN #####################################
 if __name__ == "__main__":